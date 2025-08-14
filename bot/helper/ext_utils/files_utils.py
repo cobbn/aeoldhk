1c1a63e71f44d703abc200865670d564e511ad52
cobbn <reapmr25@gmail.com>
2025-08-14 14:46:28 +0530

    test1 for \/

--- bot/helper/ext_utils/files_utils.py
+++ bot/helper/ext_utils/files_utils.py
@@ -287,7 +287,7 @@ async def process_file(file_, user_id, dirpath=None, isMirror=False):
     if remname:
         if not remname.startswith('|'):
             remname = f"|{remname}"
-        remname = remname.replace('\s', ' ')
+    remname = remname.replace(r'\s', ' ')
         slit = remname.split("|")
         __newFileName = ospath.splitext(file_)[0]
         for rep in range(1, len(slit)):
@@ -303,13 +303,13 @@ async def process_file(file_, user_id, dirpath=None, isMirror=False):
 
     nfile_ = file_
     if prefix:
-        nfile_ = prefix.replace('\s', ' ') + file_
-        prefix = re_sub(r'<.*?>', '', prefix).replace('\s', ' ')
+        nfile_ = prefix.replace(r'\s', ' ') + file_
+        prefix = re_sub(r'<.*?>', '', prefix).replace(r'\s', ' ')
         if not file_.startswith(prefix):
             file_ = f"{prefix}{file_}"
 
     if suffix and not isMirror:
-        suffix = suffix.replace('\s', ' ')
+        suffix = suffix.replace(r'\s', ' ')
         sufLen = len(suffix)
         fileDict = file_.split('.')
         _extIn = 1 + len(fileDict[-1])
@@ -319,14 +319,14 @@ async def process_file(file_, user_id, dirpath=None, isMirror=False):
             _newExtFileName = (_extOutName[:64 - (sufLen + _extIn)] + f"{suffix}.{fileDict[-1]}")
         file_ = _newExtFileName
     elif suffix:
-        suffix = suffix.replace('\s', ' ')
+        suffix = suffix.replace(r'\s', ' ')
         file_ = f"{ospath.splitext(file_)[0]}{suffix}{ospath.splitext(file_)[1]}" if '.' in file_ else f"{file_}{suffix}"
 
     cap_mono = nfile_
     if lcaption and dirpath and not isMirror:
         def lowerVars(match):
             return f"{{{match.group(1).lower()}}}"
-        lcaption = lcaption.replace('\|', '%%').replace('\{', '&%&').replace('\}', '$%$').replace('\s', ' ')
+    lcaption = lcaption.replace(r'\|', '%%').replace(r'\{', '&%&').replace(r'\}', '$%$').replace(r'\s', ' ')
         slit = lcaption.split("|")
         slit[0] = re_sub(r'\{([^}]+)\}', lowerVars, slit[0])
         up_path = ospath.join(dirpath, prefile_)
