50d2a12fa7c8d69b91b051c12a5e67580392f0c2
cobbn <reapmr25@gmail.com>
2025-08-17 13:37:59 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -222,7 +222,7 @@ def get_readable_message():
             msg += f"\n<b>user:</b> {source(download)}"
             if hasattr(download, 'seeders_num'):
                 try:
-                    msg += f"<b>| s/ʟ:</b> {download.seeders_num()}/{download.leechers_num()}"
+                    msg += f"<b>| S/L:</b> {download.seeders_num()}/{download.leechers_num()}"
                 except:
                     pass
         elif download.status() == MirrorStatus.STATUS_SEEDING:
